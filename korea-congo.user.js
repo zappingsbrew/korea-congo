787b4ddb02713a364134eff9a82a81de0add03dc
Zappingsbrew <97088485+zappingsbrew@users.noreply.github.com>
2026-01-28 13:34:57 +0800

    Update korea-congo.user.js

--- korea-congo.user.js
+++ korea-congo.user.js
@@ -14,7 +14,7 @@
 /*!
  * MIT License
  *
- * Copyright (c) 2025 zappingsbrew
+ * Copyright (c) 2026 Zappingsbrew
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
